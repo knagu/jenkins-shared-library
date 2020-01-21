d10071bf1d00c206657303530c8d26d1e5921179
knagu <45361985+knagu@users.noreply.github.com>
2020-01-21 14:01:12 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -1,9 +1,12 @@
 def call(Map config=[:]) {
 node 
   {
+	  parameters{
+		  string(defaultValue: "https://github.com/knagu/game-of-life.git", description: 'What environment?', name: 'gitUrl')
+	  }	 
 	  stage('GitSCM')
 	  {
-		  git url: 'https://github.com/knagu/game-of-life.git'
+		  git url: ${params.gitUrl}
 	  }
     	  stage('Build Stage')
 	  {
