956ca69d99a9c7ecd9cac873149b8bf6224f2b6f
knagu <45361985+knagu@users.noreply.github.com>
2020-01-22 12:22:23 +0530

    Update srcexample.groovy

--- src/org/example/srcexample.groovy
+++ src/org/example/srcexample.groovy
@@ -1,1 +1,21 @@
-
+def call(Map config=[:]) {
+node 
+  {
+	  //properties([parameters([string(defaultValue: 'https://github.com/knagu/game-of-life.git', description: '', name: 'gitUrl', trim: false)])])	 
+	  //stage('GitSCM')
+	  //{
+	//	  git url: 'https://github.com/knagu/game-of-life.git'
+	  //}	  
+    	  stage('Build Stage')
+	  {
+	   def mvnHome = tool 'M2'
+	   sh "${mvnHome}/bin/mvn -B clean install"    
+	  }
+	  stage('Test'){
+		  if(config.Test)
+		  echo "Tests successful"
+	  }
+	 
+	 
+  }
+}
