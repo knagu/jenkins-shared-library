d4cdba9b3d41ad4631ceaab8bcfde56a402f803c
knagu <45361985+knagu@users.noreply.github.com>
2020-01-24 13:37:42 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -2,21 +2,22 @@ def call(Map config=[:]) {
 node 
   {
 	  //properties([parameters([string(defaultValue: 'https://github.com/knagu/game-of-life.git', description: '', name: 'gitUrl', trim: false)])])	 
-	  //stage('GitSCM')
-	  //{
-	//	  git url: 'https://github.com/knagu/game-of-life.git'
-	  //}	  
+	  stage('GitSCM')
+	  {
+		  git url: 'https://github.com/knagu/game-of-life.git'
+	  }	  
     	  stage('Build Stage')
 	  {
 	   def mvnHome = tool 'M2'
-	   sh "${mvnHome}/bin/mvn -B clean install"    
+	   sh "${mvnHome}/bin/mvn -B clean install"
+	   echo "Build Successful"
 	  }
 	  stage('Test'){
-		  if(config.Test)
 		  echo "Tests successful"
 	  }
-	 
-	 
+	  Stage('deploy') {
+	          echo "Deploy successful"
+	  }	 	 
   }
 }
     
